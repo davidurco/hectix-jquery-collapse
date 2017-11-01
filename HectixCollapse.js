8203f452e169c158059966ef8415795a2b9064ad
Hectix <kich159@icloud.com>
2017-11-01 22:18:35 +0100

    Demo - WIP

--- HectixCollapse.js
+++ HectixCollapse.js
@@ -1,46 +1,53 @@
-   (function($) {
-       $.fn.HectixCollapse = function(options) {
+(function($) {
+    $.fn.HectixCollapse = function(options) {
 
-           var settings = $.extend({ //
-               visibleElements: 1,   //
-               animationSpeed: 400,  // Settings
-               itemsPerClick: 1   //
-           }, options);
+        var settings = $.extend({
+            visible_producers: 1,
+            animation_speed: 400,
+            items_per_click: 1,
+            show_counter: false,
+            button_text: "Next"
+        }, options);
 
 
-           var $myClass = $(this).find('.yourElements'); // find all your collapse elements
-           var $obj = $(this);
+        var $myClass = $(this).find('.producer');
+        var $obj = $(this);
 
-           var items = function($duration, $test, $type) {  // this function will collapse elements by their index.
-               $test.each(function(index) {
-                   if ($type == 'myClass') {
-                       if (index > settings.visibleProducers - 1) {
-                           $(this).slideToggle($duration);
-                       }
-                   }
-                   if ($type == 'myHidden') {
-                       if (index < settings.itemsPerClick) {
-                           $(this).slideToggle($duration);
-                       }
-                   }
-               });
-           };
+        var items = function($duration, $test, $type) {
+            $test.each(function(index) {
+                if ($type == 'myClass') {
+                    if (index > settings.visible_producers - 1) {
+                        $(this).slideToggle($duration);
+                    }
+                }
+                if ($type == 'myHidden') {
+                    if (index < settings.items_per_click) {
+                        $(this).slideToggle($duration);
+                    }
+                }
+            });
+        };
 
-           items(0, $myClass, 'myClass'); // calling first function
+        items(0, $myClass, 'myClass');
 
-           var $myHidden = $obj.find('.yourElements').filter(":hidden");  // find number of all hidden elements - before click
-           $(".left-to-show").html($myHidden.length);                // show number of all hidden elements - before click
+        var $myHidden = $obj.find('.producer').filter(":hidden");
+        if (settings.show_counter === true) {
+            $(".left-to-show").html($myHidden.length);
+        }
 
-           $(this).find('.button').on("click", function() {  // function onclick
-               items(settings.animationSpeed, $myHidden, 'myHidden'); // calling first function on click
-               $myHidden = $obj.find('.yourElements').filter(":hidden");
-               var length = $myHidden.length;
-               if (length === 0) {     // if index is equal to 0, write something (this text will be displayed in button)
-                 $('.button').text('All displayed');
-               } else { // else, show how many elements are left to display.
-                 $(".left-to-show").html(length);
-               }
-           });
-           return this;
-       };
-   })(jQuery);
+        $(".button-text").html(settings.button_text);
+
+        $(this).find('a[role="button"]').on("click", function() {
+            var $myHidden = $obj.find('.producer').filter(":hidden");
+            if (settings.show_counter === true) {
+                if ($myHidden.length === 0) {
+                    $(".left-to-show").html($myHidden.length);
+                } else {
+                    $(".left-to-show").html($myHidden.length - 1);
+                }
+            }
+            items(settings.animation_speed, $myHidden, 'myHidden');
+        });
+        return this;
+    };
+})(jQuery);
