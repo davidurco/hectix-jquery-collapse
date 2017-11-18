9f28a33a162f5946ccd4bd5934476eb53a2105af
Hectix <kich159@icloud.com>
2017-11-18 15:42:39 +0100

    Review

--- src/HectixCollapse.js
+++ src/HectixCollapse.js
@@ -1,16 +1,17 @@
 (function($) {
-    $.fn.HectixCollapse = function(options) {
+    $.fn.hectixCollapse = function(options) {
 
+        // default values for all settings
         var settings = $.extend({
             initialItems: 1,
-            slideSpeed: 400,
             itemsPerClick: 1,
+            slideSpeed: 400,
             showCounter: false,
             buttonText: "Next"
         }, options);
 
-        var collapse_item = $(this).find('.collapse-item');
         var $obj = $(this);
+        var collapse_item = $obj.find('.collapse-item');
 
         var items = function($duration, $collapse_item, $type) {
             $collapse_item.each(function(index) {
@@ -28,21 +29,21 @@
         };
 
         items(0, collapse_item, 'initial');
-        $(this).removeClass("cs-hidden");
+        $obj.removeClass("cs-hidden");
         var hiddenItem = $obj.find('.collapse-item').filter(":hidden");
+
         if (settings.showCounter === true) {
-            $(this).find(".counter").html(hiddenItem.length);
+            $obj.find(".counter").html(hiddenItem.length);
         }
+        $obj.find(".button-text").html(settings.buttonText);
 
-        $(this).find(".button-text").html(settings.buttonText);
-
-        $(this).find('a.collapse-button').on("click", function() {
+        $obj.find('a.collapse-button').on("click", function() {
             var hiddenItem = $obj.find('.collapse-item').filter(":hidden");
             if (settings.showCounter === true) {
                 if (hiddenItem.length === 0) {
-                    $(this).find(".counter").html(hiddenItem.length);
+                    $obj.find(".counter").html(hiddenItem.length);
                 } else {
-                    $(this).find(".counter").html(hiddenItem.length - 1);
+                    $obj.find(".counter").html(hiddenItem.length - 1);
                 }
             }
             items(settings.slideSpeed, hiddenItem, 'progress');
